1347b5cc64e865c245de3acde4ed1e923d445e10
linus-vanpelt919 <thelastwezardof@gmail.com>
2023-09-06 19:14:20 +0900

    add: CRUD

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'this is  itemsService';
+  private items: Item[] = [];
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+
+  delete(id: string): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
